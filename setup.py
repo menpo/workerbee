877fd8660c99ef09b042c715de558f01c99437ff
James Booth <jabooth@gmail.com>
2016-08-07 10:26:09 +0100

    upgrade to new versioneer

--- setup.py
+++ setup.py
@@ -2,17 +2,6 @@ from setuptools import setup, find_packages
 import sys
 import versioneer
 
-project_name = 'workerbee'
-
-# Versioneer allows us to automatically generate versioning from
-# our git tagging system which makes releases simpler.
-versioneer.VCS = 'git'
-versioneer.versionfile_source = '{}/_version.py'.format(project_name)
-versioneer.versionfile_build = '{}/_version.py'.format(project_name)
-versioneer.tag_prefix = 'v'  # tags are like v1.2.0
-versioneer.parentdir_prefix = project_name + '-'  # dirname like 'menpo-v1.2.0'
-
-
 requirements = [
       'postgres>=2.2.1',
       'psycopg2>=2.6.1',
@@ -21,7 +10,7 @@ requirements = [
 if sys.version_info.major == 2:
     requirements.extend(['pathlib>=1.0'])
 
-setup(name=project_name,
+setup(name='workerbee',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
       description='Simple functional framework for embarrassingly parallel jobs',
